ead38edcc26209f42302cf5c3d0d666f5b1fbc3c
eprincev.egor@yandex.ru <eprincev.egor@yandex.ru>
2017-07-30 13:15:26 +0300

    new images

--- js/App.js
+++ js/App.js
@@ -69,7 +69,7 @@ class App {
             i, j, l,
             stack = [],
             rndIndex, rndAnimal,
-            animals = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38];
+            animals = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,16];
         
         for (i=0, l = n * m / 2; i<l; i++) {
             rndIndex = Math.floor( Math.random() * animals.length );
